643106c33ad56b66ed1607a37626ebcce7cf74ee
harapeco-mgn <harapeco.mgn@gmail.com>
2026-02-07 20:01:17 +0900

    HareEntry 編集・更新機能を実装（ルーティング・コントローラー）

--- app/controllers/hare_entries_controller.rb
+++ app/controllers/hare_entries_controller.rb
@@ -1,12 +1,12 @@
 class HareEntriesController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_hare_entry, only: [:show, :edit, :update]
 
     def index
       @hare_entries = current_user.hare_entries.order(created_at: :desc)
     end
 
     def show
-      @hare_entry = current_user.hare_entries.find(params[:id])
     end
 
     def new
@@ -23,7 +23,22 @@ class HareEntriesController < ApplicationController
       end
     end
 
+    def edit
+    end
+
+    def update
+      if @hare_entry.update(hare_entry_params)
+        redirect_to hare_entry_path(@hare_entry), notice: "ハレの記録を更新しました"
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
     private
+
+    def set_hare_entry
+      @hare_entry = current_user.hare_entries.find(params[:id])
+    end
 
     def hare_entry_params
       params.require(:hare_entry).permit(:body, :occurred_on, :visibility)
