06cf1cc2f1a4d74758d81063131a491ee10f21b3
harapeco-mgn <harapeco.mgn@gmail.com>
2026-02-07 21:31:15 +0900

    HareEntry 削除機能を実装（TDD）

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
 
-  resources :hare_entries, only: [ :index, :show, :new, :create, :edit, :update ]
+    resources :hare_entries, only: [ :index, :show, :new, :create, :edit, :update, :destroy ]
 end
