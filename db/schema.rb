289c8cf9aaed370552482f8391489ec46c422ace
harapeco-mgn <harapeco.mgn@gmail.com>
2026-02-10 14:31:23 +0900

    schema.rb 更新とサマリファイル削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_09_103706) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_10_051447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_09_103706) do
   end
 
   create_table "hare_entries", force: :cascade do |t|
+    t.integer "awarded_points", default: 0, null: false
     t.text "body", null: false
     t.datetime "created_at", null: false
     t.date "occurred_on", null: false
